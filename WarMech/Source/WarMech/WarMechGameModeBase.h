f84b9ae20be25c0228284e27bd84bf5cb3f9f01a
DESKTOP-U8R4R54\Lazurus <kirilllazutin@mail.ru>
2017-05-08 19:14:50 +0300

    + *VC.db

---
+++ WarMech/Source/WarMech/WarMechGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "WarMechGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARMECH_API AWarMechGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
